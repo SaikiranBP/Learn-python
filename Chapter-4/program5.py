bda8917237de042d3e6450592bd1b19953fb799e
SaikiranBP <saikiranbpujari@gmail.com>
2024-05-09 16:56:51 +0530

    adding more conditions to the code

--- Chapter-4/program5.py
+++ Chapter-4/program5.py
@@ -10,27 +10,45 @@ for i in range(1, user_input + 1):
     input_list = input()
     stationary_list.append(input_list)
 
-print('Your list contains ' + str(stationary_list))  # prints the final list of items
-print("")
-print('Do you want to remove an item from the list randomly or remove the last item from the list?') # asking the user whehter to remove an item from the list 
-print("Type 'random' to remove random item from the list")
-print("Type 'last item' to remove item which is at the end of the list")
-user_answer = input()
-user_lowerletter = user_answer.lower()  # converts the users input into lower letters
+print('Your list contains ' + str(stationary_list))  # prints the final list of items 
 
 # if user_answer is random, the word will be removed from the list randomly, 
 # if user_answer is last item then the last item will be removed from the list
 
-if user_lowerletter == 'random':
-      remove_word = ''.join(random.choices(stationary_list, k=1))
-      stationary_list.remove(remove_word)
-      print('The item which is removed from the list is: ' + str(remove_word))
-      print('The final list contains ' + str(stationary_list))
+while True:
+    print()
+    print("Type 'random' to remove random item from the list") 
+    print("Type 'last item' to remove item which is at the end of the list")
+    print("Type 4 to exit")
 
-elif user_lowerletter == 'last item':
-    del stationary_list[user_input - 1]
-    print("The final list contains: " + str(stationary_list))
+    user_answer = input()
+    user_lowerletter = user_answer.lower() # converts the users input into lower letters
 
-else:
-     print("Please type correct word")
-    
+    if user_lowerletter == 'random':
+        remove_word = ''.join(random.choices(stationary_list, k=1))
+        stationary_list.remove(remove_word)
+        print("The list contains: " + str(stationary_list))
+        print("To view the popped item Type pop")
+        pop_word = input()
+        if pop_word == 'pop':
+            print(remove_word)
+            continue
+        else:
+            continue
+
+    elif user_lowerletter == 'last item':
+        deleted_word = stationary_list.pop()
+        print("Your final list contains: " + str(stationary_list))
+        print("To view the popped item Type pop")
+        pop_word1 = input()
+        if pop_word1 == 'pop':
+            print(deleted_word)
+            continue
+        else:
+            continue
+
+    elif user_lowerletter == '4':
+        break
+
+    else:
+        continue
