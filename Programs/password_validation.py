a9829db7d753b043a793f13dc7cc5fc52a88e115
SaikiranBP <saikiranbpujari@gmail.com>
2024-07-15 09:44:39 +0530

    writing a program for validation of password

---
+++ Programs/password_validation.py
@@ -0,0 +1,34 @@
+"""
+This program is written using regular expressions.
+This program validates the password if all the given conditions satisfy.
+Conditions:
+    1. The password should contain minimum 8 characters
+    2. The password should contain uppercase letters, lowercase letters,
+       special charcters and numbers.
+    3. The password should start with uppercase letter
+"""
+import re
+
+def validate_password():
+    if not re.search("^[A-Z]", password):
+        return "Password must start with uppercase letter"
+    
+    if not re.search("[a-z]", password):
+        return "Password must contain lowercase letters"
+    
+    if not re.search("[A-Z]", password):
+        return "Password must contain uppercase letters"
+    
+    if len(password) < 8:
+        return "Password must contain minimum 8 characters"
+    
+    if not re.search("\d", password):
+        return "Password must contain numbers"
+    
+    if not re.search("[!@#$%^&*()_-]", password):
+        return "Password must contain atleast one special character"
+    
+    return "Password validation successful"
+
+password = input("Type the password: ")
+print(validate_password())
