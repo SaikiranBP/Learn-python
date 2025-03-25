edc333eb53eae334b77e852557c24873b6b6cb1d
SaikiranBP <saikiranbpujari@gmail.com>
2025-03-25 20:19:26 +0530

    writing a function to list out the players accoring to the roles

--- Projects/Cricket/main.py
+++ Projects/Cricket/main.py
@@ -22,6 +22,17 @@ def show_list(any_list):
 def toss():
     return (random.choice(['H', 'T'])) 
 
+def fetch_players(cric_data, team, role):
+    players_ls = []
+    print(role, end=" ")
+    for t in cric_data.get('teams'):
+        if t['Country'] == team:
+            for player in t['Players']:
+                if player['role'] == role:
+                    players_ls.append(player['name'])
+    return players_ls
+
 cricket_details = load_data()
 country_list = fetch_countries(cricket_details)
 display_countries = show_list(country_list)
+get_players_by_role = fetch_players(cricket_details, "India", "Batsmna")
