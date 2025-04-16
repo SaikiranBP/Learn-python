55d69a3240ba02cf3ae135058bae5bb9e6a6dcb8
SaikiranBP <saikiranbpujari@gmail.com>
2025-04-16 18:37:32 +0530

    performing toss

--- Projects/Cricket/main.py
+++ Projects/Cricket/main.py
@@ -23,16 +23,49 @@ def toss():
     return (random.choice(['H', 'T'])) 
 
 def fetch_players(cric_data, team, role):
-    players_ls = []
-    print(role, end=" ")
+    players_with_role = []
+    print(role)
     for t in cric_data.get('teams'):
         if t['Country'] == team:
             for player in t['Players']:
                 if player['role'] == role:
-                    players_ls.append(player['name'])
-    return players_ls
+                    players_with_role.append(player['name'])
+    return players_with_role
+
+def choose_team(any_list):
+    any_map = {}
+    sl_no = 1
+    for a in any_list:
+        any_map[sl_no] = a
+        sl_no += 1
+    for k, v in any_map.items():
+        print(f"{k} - {v}")
+    choice = int(input("Enter the number corresponding to your choice: "))
+    for x, y in any_map.items():
+        if choice == x:
+            print(f"You Chose {y} as your team")
+
+def perform_toss():
+    toss_result = toss()
+    print("Choose H for Heads, T for Tails, E for exit")
+    user_call = input().upper()
+    if user_call == "E":
+        print("Exiting the program, have a good day")
+        exit()
+    elif user_call in ["H", "T"]:
+        print(f"Toss result: {toss_result}")
+        if user_call == toss_result:
+            print("You have won the toss")
+        else:
+            print("You have lost the toss")
+    else:
+        print("You have entered the wrong choice")
 
 cricket_details = load_data()
 country_list = fetch_countries(cricket_details)
-display_countries = show_list(country_list)
-get_players_by_role = fetch_players(cricket_details, "India", "Batsman")
+
+
+print("------WELCOME TO CRICKET------")
+print("Choose your team")
+choose_team(country_list)
+perform_toss()
